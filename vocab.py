1dba87f0ccfed507308e6f1a6d6b2e957ce5beec
jaeminkim <darkkjm2002@gmail.com>
2019-05-18 16:34:53 +0900

    Develop

---
+++ vocab.py
@@ -0,0 +1,32 @@
+import gensim
+import logging
+import pandas as pd
+import json
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+def extract_questions():
+    """
+    Extract questions for making word2vec model.
+    """
+    df1 = pd.read_csv("./data_in/snli_1.0_train.txt")
+    df2 = pd.read_csv("./data_in/snli_1.0_text.txt")
+
+    for dataset in [df1, df2]:
+        for i, row in dataset.iterrows():
+            if i != 0 and i % 1000 == 0:
+                logging.info("read {0} sentences".format(i))
+
+            if row['question1']:
+                yield gensim.utils.simple_preprocess(row['question1'])
+            if row['question2']:
+                yield gensim.utils.simple_preprocess(row['question2'])
+
+
+documents = list(extract_questions())
+logging.info("Done reading data file")
+
+model = gensim.models.Word2Vec(documents, size=300)
+model.train(documents, total_examples=len(documents), epochs=10)
+model.save("./data/Quora-Question-Pairs.w2v")
