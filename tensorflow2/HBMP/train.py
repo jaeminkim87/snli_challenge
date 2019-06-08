aaaab39acee231181cf4e0e45f09538a69840489
jaeminkim87 <rlawoals2!>
2019-06-09 02:46:27 +0900

    Develop MaLSTM, HBMP with Tensorflow 2

--- tensorflow2/HBMP/train.py
+++ tensorflow2/HBMP/train.py
@@ -1,40 +1,42 @@
-import tensorflow as tf
 import pickle
+import tensorflow as tf
 
 from pathlib import Path
 from mecab import MeCab
-from gluonnlp.data import PadSequence
 from tqdm import tqdm
-from absl import app
-from model.net import Linear
+from model.net import HBMP
 from model.data import Corpus
+from config import FLAGS
+from absl import app
 
 
-def main():
+def main(argv):
     train_path = Path.cwd() / '..' / 'data_in' / 'train.txt'
     val_path = Path.cwd() / '..' / 'data_in' / 'val.txt'
     vocab_path = Path.cwd() / '..' / 'data_in' / 'vocab.pkl'
 
-    length = 70
-    batch_size = 1024
-    learning_rate = 0.01
-    epochs = 10
+    batch_size = FLAGS.batch_size
+    learning_rate = FLAGS.learning_rate
+    epochs = FLAGS.epochs
 
     with open(vocab_path, mode='rb') as io:
         vocab = pickle.load(io)
 
-    train = tf.data.TextLineDataset(str(train_path)).shuffle(buffer_size=1000).batch(batch_size=batch_size,
+    train = tf.data.TextLineDataset(str(train_path)).shuffle(buffer_size=batch_size).batch(batch_size=batch_size,
                                                                                      drop_remainder=True)
     eval = tf.data.TextLineDataset(str(val_path)).batch(batch_size=batch_size, drop_remainder=True)
 
     tokenizer = MeCab()
-    processing = Corpus(vocab, tokenizer)
+    corpus = Corpus(vocab, tokenizer)
 
-    linear = Linear(vocab)
+    hbmp = HBMP()
 
     opt = tf.optimizers.Adam(learning_rate=learning_rate)
     loss_fn = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
 
+    '''
+    loss, accuracy
+    '''
     train_loss_metric = tf.keras.metrics.Mean(name='train_loss')
     train_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
     val_loss_metric = tf.keras.metrics.Mean(name='val_loss')
@@ -48,8 +50,25 @@ def main():
         tf.keras.backend.set_learning_phase(1)
 
         for step, val in tqdm(enumerate(train)):
-            print(val)
+            sen1, sen2, label = corpus.token2idx(val)
+            with tf.GradientTape() as tape:
+                logits = hbmp(sen1, sen2)
+                train_loss = loss_fn(label, logits)
+                print(logits)
+
+            grads = tape.gradient(target=train_loss, sources=hbmp.trainable_variables)
+            opt.apply_gradients(grads_and_vars=zip(grads, hbmp.trainable_variables))
+
+            train_loss_metric.update_state(train_loss)
+            train_acc_metric.update_state(label, logits)
+
+        tr_loss = train_loss_metric.result()
+
+        tqdm.write(
+            'epoch : {}, tr_acc : {:.3f}%, tr_loss : {:.3f}'.format(epoch + 1,
+                                                                    train_acc_metric.result() * 100,
+                                                                    tr_loss))
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    app.run(main)
