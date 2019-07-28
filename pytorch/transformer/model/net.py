d350868028945f581b2303579b16cd76e4adce10
jaeminkim87 <rlawoals2!>
2019-07-28 09:48:10 +0900

    Develop

--- pytorch/transformer/model/net.py
+++ pytorch/transformer/model/net.py
@@ -1,11 +1,44 @@
 import torch
 import copy
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.modules as Modules
 
 from model.utils import _reset_parameters
 from model.ops import TransformerEncoderLayer, TransformerDecoderLayer, TransformerEncoder, TransformerDecoder
+
+class ScheduledOptim():
+    '''A simple wrapper class for learning rate scheduling'''
+
+    def __init__(self, optimizer, d_model, n_warmup_steps):
+        self._optimizer = optimizer
+        self.n_warmup_steps = n_warmup_steps
+        self.n_current_steps = 0
+        self.init_lr = np.power(d_model, -0.5)
+
+    def step_and_update_lr(self):
+        "Step with the inner optimizer"
+        self._update_learning_rate()
+        self._optimizer.step()
+
+    def zero_grad(self):
+        "Zero out the gradients by the inner optimizer"
+        self._optimizer.zero_grad()
+
+    def _get_lr_scale(self):
+        return np.min([
+            np.power(self.n_current_steps, -0.5),
+            np.power(self.n_warmup_steps, -1.5) * self.n_current_steps])
+
+    def _update_learning_rate(self):
+        ''' Learning rate scheduling per step '''
+
+        self.n_current_steps += 1
+        lr = self.init_lr * self._get_lr_scale()
+
+        for param_group in self._optimizer.param_groups:
+            param_group['lr'] = lr
 
 
 class Transformer(nn.Module):
@@ -36,14 +69,11 @@ class Transformer(nn.Module):
         self.len_vocab = len(vocab)
         self.nhead = nhead
 
-        self.dropout = nn.Dropout(p=args.dropout)
-        self.dense2 = nn.Linear(self.d_model, 1)
-        self.dense3 = nn.Linear(self.d_model, args.classes)
-        self.max_pool = nn.MaxPool1d(args.word_max_len)
-
-        self.dense_1 = nn.Linear(128, 1)
-        self.dense_3 = nn.Linear(128, 3)
-        self.relu = nn.ReLU(inplace=True)
+        self.dropout1 = nn.Dropout(p=args.dropout)
+        self.layout1 = nn.Linear(args.word_max_len * self.d_model * 4, args.hidden_size * 2)
+        self.dropout2 = nn.Dropout(p=args.dropout)
+        self.layout2 = nn.Linear(args.hidden_size * 2, args.hidden_size)
+        self.layout3 = nn.Linear(args.hidden_size, args.classes)
 
     def forward(self, src, target, src_mask=None, target_mask=None,
                 memory_mask=None, src_key_padding_mask=None, target_key_padding_mask=None,
@@ -57,21 +87,22 @@ class Transformer(nn.Module):
         if src.size(2) != self.d_model or target.size(2) != self.d_model:
             raise RuntimeError("the feature number of src and target must be equal to d_model")
 
-
-        prem = self.encoder(src, mask=src_mask, src_key_padding_mask=src_key_padding_mask)
+        prem = self.encoder(src, mask=src_mask, src_key_padding_mask=src_key_padding_mask) # [256, 30, 128]
         hypo = self.encoder(target, mask=target_mask, src_key_padding_mask=target_key_padding_mask)
 
-        prem = prem.view(self.batch_size, self.d_model, -1)
-        similarity = prem @ hypo
+        prem = prem.view(prem.size()[0], -1)
+        hypo = hypo.view(hypo.size()[0], -1)
 
-        similarity = self.dense_1(similarity)
-        similarity = similarity.view(self.batch_size, -1)
-        similarity = self.relu(similarity)
+        output = torch.cat([prem, hypo, torch.abs(prem - hypo), prem * hypo], 1)
 
-        out = self.dense_3(similarity)
+        output = self.dropout1(output)
+        output = F.relu(self.layout1(output))
+        output = self.dropout2(output)
+        output = F.relu(self.layout2(output))
+        output = self.layout3(output)
+        return output
 
-        return out
-
+        # return out
 
     def generate_square_subsequent_mask(self, sz):
         mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
--- pytorch/transformer/train.py
+++ pytorch/transformer/train.py
@@ -3,6 +3,7 @@ import argparse
 import torch
 import torch.nn as nn
 import nltk
+import numpy as np
 import matplotlib.pyplot as plt
 
 from torch.autograd import Variable
@@ -12,7 +13,7 @@ from torch import optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader
 from gluonnlp.data import PadSequence
-from model.net import Transformer
+from model.net import Transformer, ScheduledOptim
 from model.data import Tokenizer, Corpus
 from model.metric import evaluate, acc
 from torch.nn.utils import clip_grad_norm_
@@ -26,8 +27,8 @@ from preprocessing.build_vocab import Build_Vocab
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--epoch', default=10, type=int)
-    parser.add_argument('--batch_size', default=256, type=int)
-    parser.add_argument('--hidden_size', default=256, type=int)
+    parser.add_argument('--batch_size', default=64, type=int)
+    parser.add_argument('--hidden_size', default=64, type=int)
     # parser.add_argument('--data_type', default='senCNN')
     parser.add_argument('--dropout', default=0.1, type=int)
     parser.add_argument('--classes', default=3, type=int)
@@ -35,7 +36,7 @@ def main():
     parser.add_argument('--learning_rate', default=1e-3, type=float)
     # parser.add_argument('--print_freq', default=3000, type=int)
     # parser.add_argument('--weight_decay', default=5e-5, type=float)
-    parser.add_argument('--word_dim', default=128, type=int)
+    parser.add_argument('--word_dim', default=64, type=int)
     parser.add_argument('--word_max_len', default=30, type=int)
     parser.add_argument('--global_step', default=1000, type=int)
     parser.add_argument('--data_path', default='../data_in')
@@ -47,8 +48,8 @@ def main():
     # p = Preprocessing(args)
     # p.make_datafile()
 
-    v = Build_Vocab(args)
-    v.make_vocab()
+    # v = Build_Vocab(args)
+    # v.make_vocab()
 
     with open(args.data_path + '/' + 'vocab.pkl', mode='rb') as io:
         vocab = pickle.load(io)
@@ -63,84 +64,81 @@ def main():
     val_ds = Corpus(args.data_path + '/val.txt', tokenizer.split_and_transform)
     val_dl = DataLoader(val_ds, batch_size=args.batch_size)
 
+    test_ds = Corpus(args.data_path + '/test.txt', tokenizer.split_and_transform)
+    test_dl = DataLoader(test_ds, batch_size=args.batch_size)
+
     dev_ds = Corpus(args.data_path + '/dev.txt', tokenizer.split_and_transform)
     dev_dl = DataLoader(dev_ds, batch_size=args.batch_size, shuffle=True, num_workers=4, drop_last=True)
 
-    opt = optim.Adam(params=model.parameters(), lr=args.learning_rate)
+    # opt = optim.Adam(params=model.parameters(), lr=args.learning_rate)
+    opt = ScheduledOptim(
+        torch.optim.Adam(filter(lambda x: x.requires_grad, model.parameters()), betas=(0.9, 0.98), eps=1e-09),
+        args.word_dim, 4000)
     loss_fn = nn.CrossEntropyLoss()
-    scheduler = ReduceLROnPlateau(opt, patience=5)
+    # scheduler = ReduceLROnPlateau(opt, patience=5)
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     model.to(device)
 
     best_val_loss = 1e+10
 
+    epochs = []
+    valid_score = []
+    test_score = []
+
+    min_loss = 100
+    max_dev = 0
+    max_test = 0
+
     for epoch in tqdm(range(args.epoch), desc='epochs'):
         tr_loss = 0
         tr_acc = 0
         model.train()
-        for step, mb in tqdm(enumerate(dev_dl), desc='steps', total=len(dev_dl)):
+        for step, mb in tqdm(enumerate(tr_dl), desc='tr', total=len(tr_dl)):
             sent1, sent2, label = map(lambda elm: elm.to(device), mb)
             opt.zero_grad()
             logits = model(sent1, sent2)
+            _, indices = logits.max(1)
             mb_loss = loss_fn(logits, label)
             mb_loss.backward()
-            # clip_grad_norm_(model._fc.weight, 5)
-            opt.step()
-
-            with torch.no_grad():
-                mb_acc = acc(logits, label)
-
+            # clip_grad_norm_(model.layout3.weight, 5)
+            # opt.step()
+            opt.step_and_update_lr()
             tr_loss += mb_loss.item()
-            tr_acc += mb_acc.item()
+        train_loss = tr_loss / len(tr_dl)
+        model.eval()
 
-            # if (epoch * len(tr_dl) + step) % args.global_step == 0:
-            #     print("epoch : ", epoch + 1, " step : ", step, " loss : ", tr_loss.item(), "acc : ", tr_acc.item())
-            #     val_loss = evaluate(model, val_dl, {'loss': loss_fn}, device)['loss']
-            #     # writer.add_scalars('loss', {'train': tr_loss / (step + 1),
-            #     #                             'val': val_loss}, epoch * len(tr_dl) + step)
-            #     model.train()
+        valid_match = []
+        for step, mb in tqdm(enumerate(val_dl), desc='val', total=len(val_dl)):
+            sent1, sent2, label = map(lambda elm: elm.to(device), mb)
+            logits = model(sent1, sent2)
+            _, indices = logits.max(1)
+            match = torch.eq(indices, label).detach()
+            valid_match.extend(match.cpu())
+        valid_accuracy = np.sum(valid_match) * 100 / len(valid_match)
+        valid_score.append(valid_accuracy)
 
-        else:
-            tr_loss /= (step + 1)
-            tr_acc /= (step + 1)
+        min_loss = min(min_loss, train_loss)
+        max_dev = max(max_dev, valid_accuracy)
 
-            tr_summ = {'loss': tr_loss, 'acc': tr_acc}
-            # val_summ = evaluate(model, val_dl, {'loss': loss_fn, 'acc': acc}, device)
-            # scheduler.step(val_summ['loss'])
-            tqdm.write('epoch : {}, tr_loss: {:.3f}, tr_acc: {:.2%}'.format(epoch + 1, tr_summ['loss'],
-                                                                        tr_summ['acc']))
-            # tqdm.write('epoch : {}, tr_loss: {:.3f}, val_loss: '
-            #            '{:.3f}, tr_acc: {:.2%}, val_acc: {:.2%}'.format(epoch + 1, tr_summ['loss'],
-            #                                                             val_summ['loss'],
-            #                                                             tr_summ['acc'], val_summ['acc']))
+        tqdm.write("[%2d], loss: %.3f, val_acc: %.3f" % (epoch + 1, train_loss, valid_accuracy))
 
-            # val_loss = val_summ['loss']
-            # is_best = val_loss < best_val_loss
+        #
+        # test evaluate
+        #
+        test_match = []
+        for step, mb in tqdm(enumerate(test_dl), desc='val', total=len(test_dl)):
+            sent1, sent2, label = map(lambda elm: elm.to(device), mb)
+            logits = model(sent1, sent2)
+            _, indices = logits.max(1)
+            match = torch.eq(indices, label).detach()
+            test_match.extend(match.cpu())
+        test_accuracy = np.sum(test_match) * 100 / len(test_match)
+        test_score.append(test_accuracy)
 
-            # if is_best:
-            #     state = {'epoch': epoch + 1,
-            #              'model_state_dict': model.state_dict(),
-            #              'opt_state_dict': opt.state_dict()}
-            #     summary = {'tr': tr_summ, 'val': val_summ}
 
-            # manager.update_summary(summary)
-            # manager.save_summary('summary.json')
-            # manager.save_checkpoint(state, 'best.tar')
+        max_test = max(max_test, test_accuracy)
 
-            #    best_val_loss = val_loss
-
-    # Loss 그래프
-    plt.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], tr_summ['loss'], 'r--')
-    plt.legend(['Training Loss'])
-    plt.xlabel('Step')
-    plt.ylabel('Loss')
-    plt.show()
-
-    # Acc 그래프
-    plt.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], tr_summ['acc'], 'b--')
-    plt.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], val_summ['acc'], 'g--')
-    plt.legend(['Tr acc', 'Val acc'])
-    plt.show()
+        tqdm.write("[%2d], test_acc: %.3f" % (epoch + 1, test_accuracy))
 
 
 if __name__ == '__main__':
